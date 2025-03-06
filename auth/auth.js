216655042868c55c85eeafa9ce75c3c380908076
thuanAlex30 <thuandh30@gmail.com>
2025-03-06 13:15:03 +0700

    ok

--- auth/auth.js
+++ auth/auth.js
@@ -10,37 +10,33 @@ passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-    profileFields: ['id', 'displayName', 'email']
+    profileFields: ['id', 'displayName', 'emails']  // 'displayName' is part of the profile
 }, async (accessToken, refreshToken, profile, done) => {
-    console.log("Access Token:", accessToken);  // Log access token to verify
-    console.log("Refresh Token:", refreshToken);  // Log refresh token to verify
-    console.log("Facebook Profile:", profile);  // Log Facebook profile data
-
     try {
         let user = await User.findOne({ facebookId: profile.id });
         if (!user) {
             user = new User({
                 facebookId: profile.id,
-                name: profile.displayName,
-                email: profile.emails ? profile.emails[0].value : null
+                name: profile.displayName,  // 'name' saved here
+                email: profile.emails ? profile.emails[0].value : "N/A"
             });
             await user.save();
         }
-        return done(null, user);
-    } catch (error) {
-        console.error("Error in Facebook authentication:", error);
-        return done(error, null);
+        return done(null, user);  // Pass the user object to 'done'
+    } catch (err) {
+        return done(err, null);
     }
 }));
 
-
-
 passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id);
+    console.log("User found:", user);  // Log the user object
     done(null, user);
 });
 
-passport.deserializeUser((obj, done) => {
-    done(null, obj);
-});
 
 module.exports = passport;
